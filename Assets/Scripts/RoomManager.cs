1852ba2dd4a257b3968a4b4805199ea44ba331ab
도준영 <kaco2301@gmail.com>
2024-02-25 00:37:15 +0900

    0224

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager instance;
+    public GameObject player;
+
+    [Space]
+    public Transform spawnPoint;
+
+    [Space]
+    public GameObject roomCam;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        Debug.Log("Connecting");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        Debug.Log("We're in the lobby");
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("We're connected and in a room now");
+
+        roomCam.SetActive(false);
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+
+    public void RespawnPlayer()
+    {
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+
+}
